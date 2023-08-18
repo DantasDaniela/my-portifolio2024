dc6a696919dad21de9b01e21ba2622c69e5f83b2
Daniela Dantas de Lima <114183551+DantasDaniela@users.noreply.github.com>
2023-08-18 07:55:15 -0300

    link instagram
    ajustando os endereços de links

--- src/componentes/sections/Navbar.js
+++ src/componentes/sections/Navbar.js
@@ -11,7 +11,7 @@ function Navbar(){
                 
             </ul>
             <ul>
-                <li><a href='https://www.instagram.com/accounts/login/'><FaInstagram size={(30)}/></a></li>
+                <li><a href='https://www.instagram.com/danielaaugusto83/'><FaInstagram size={(30)}/></a></li>
                 <li><a href='https://github.com/DantasDaniela'><FaGithub size={(30)}/></a></li>
                 <li><a href='https://www.linkedin.com/company/login/?https://www.linkedin.com/in/https://www.linkedin.com/in/daniela-dantas-175507255-dantas-175507255/=br'><FaLinkedin size={(30)}/></a></li>
             </ul>
